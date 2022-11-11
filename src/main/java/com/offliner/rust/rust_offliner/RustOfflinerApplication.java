72499e476e8e26ecb6ddc23cfa8d3041baa9a6bb
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2022-11-11 15:56:39 +0100

    initial commit

---
+++ src/main/java/com/offliner/rust/rust_offliner/RustOfflinerApplication.java
@@ -0,0 +1,13 @@
+package com.offliner.rust.rust_offliner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RustOfflinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RustOfflinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/offliner/rust/rust_offliner/RustOfflinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.offliner.rust.rust_offliner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RustOfflinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
