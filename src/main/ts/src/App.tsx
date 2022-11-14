6a3788835a1d2b28ee3f08080bd374c24e428d1a
A-Wodnicki <wodnicki.adrian4@gmail.com>
2022-11-14 23:27:47 +0100

    Add temporary layout for server search result and server map

--- src/main/ts/src/App.tsx
+++ src/main/ts/src/App.tsx
@@ -1,5 +1,13 @@
 import Header from './layout/header/Header'
 
 export default function App() {
-  return <Header />
+  return (
+    <>
+      <Header />
+
+      {/* temp layout */}
+      <div className='search-result'>search result</div>
+      <div className='map'>map</div>
+    </>
+  )
 }
