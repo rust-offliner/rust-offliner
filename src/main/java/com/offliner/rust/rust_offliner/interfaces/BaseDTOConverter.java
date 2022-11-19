64ef8bdf68fe735674908d74791d0cd5444eb3f6
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2022-11-19 18:47:24 +0100

    basic DTO conversion interface

---
+++ src/main/java/com/offliner/rust/rust_offliner/interfaces/BaseDTOConverter.java
@@ -0,0 +1,22 @@
+package com.offliner.rust.rust_offliner.interfaces;
+
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+//convert from database object @Entity to DTO
+//or the other way around
+public interface BaseDTOConverter<F, T> {
+
+    public T convert(F from);
+
+    default public Collection<T> convertAll(Collection<F> fElements){
+        Collection<T> convertedElement =
+                fElements.stream()
+                        .map(element -> convert(element))
+                        .collect(Collectors.toList());
+
+        return convertedElement;
+    }
+
+}
