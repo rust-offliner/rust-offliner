9d43bbeeb7e8a547e9f9d2b2435f582850b6a48b
A-Wodnicki <wodnicki.adrian4@gmail.com>
2022-11-27 18:19:18 +0100

    ui changes
    - updated formating rules to double quotes
    - reorganized layout
    - introduced style modules
    - updated file structure
    - removed yarn

---
+++ src/main/ts/src/components/main/serverMap/ServerMap.tsx
@@ -0,0 +1,5 @@
+import styles from "./serverMap.module.scss"
+
+export default function ServerMap() {
+  return <div className={styles.map}>map</div>
+}
--- src/main/ts/src/index.tsx
+++ src/main/ts/src/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './assets/scss/index.scss'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+import "./assets/styles/index.scss"
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
