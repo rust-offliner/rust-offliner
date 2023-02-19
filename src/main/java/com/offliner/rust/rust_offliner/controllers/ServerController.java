061c8336e6467cde00896e9898df4bfdb7ab5c4b
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2023-02-19 22:42:26 +0100

    cleaning unused classes

--- src/main/java/com/offliner/rust/rust_offliner/controllers/ServerController.java
+++ src/main/java/com/offliner/rust/rust_offliner/controllers/ServerController.java
@@ -3,8 +3,6 @@ package com.offliner.rust.rust_offliner.controllers;
 import com.offliner.rust.rust_offliner.datamodel.BattlemetricsServerDTO;
 import com.offliner.rust.rust_offliner.datamodel.TokenizedResponse;
 import com.offliner.rust.rust_offliner.exceptions.KeyAlreadyExistsException;
-import com.offliner.rust.rust_offliner.exceptions.ServerNotTrackedException;
-import com.offliner.rust.rust_offliner.interfaces.IServerValidator;
 import com.offliner.rust.rust_offliner.persistence.ServerDataStateManager;
 import com.offliner.rust.rust_offliner.security.TokenHandler;
 import com.offliner.rust.rust_offliner.services.BattlemetricsServerService;
@@ -18,6 +16,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.time.Duration;
 
@@ -34,9 +33,6 @@ public class ServerController {
     ServerDataStateManager manager;
     @Autowired
     TokenHandler tokenHandler;
-
-    @Autowired
-    IServerValidator serverDao;
 
     private final Bucket bucket;
 
@@ -76,19 +72,16 @@ public class ServerController {
         if (bucket.tryConsume(1)) {
             String newToken = tokenHandler.handle(authorization);
             try {
-                log.debug("endpoint wywolany");
                 manager.add(id);
-//                log.info(serverService.getServerString(id));
-                return ResponseEntity.ok(new TokenizedResponse<>(newToken, bucket.getAvailableTokens(), null));
+
+                return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri())
+                        .header("X-Rate-Limit-Remaining", String.valueOf(bucket.getAvailableTokens()))
+                        .build();
             } catch (KeyAlreadyExistsException e) {
                 return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(new TokenizedResponse<>(newToken, bucket.getAvailableTokens(), e.getMessage()));
             }
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
-
-//    @PostMapping("query")
-//    public Server returnServer(@RequestBody String query) {  }
 
 }
--- src/main/java/com/offliner/rust/rust_offliner/persistence/ServerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.offliner.rust.rust_offliner.persistence;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ServerRepository {
-}
