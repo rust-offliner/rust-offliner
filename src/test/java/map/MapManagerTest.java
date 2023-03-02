175748c67a39159c148f23f64be3c41d39ff20c5
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2023-03-02 21:30:21 +0100

    more tests

--- src/test/java/map/MapManagerTest.java
+++ src/test/java/map/MapManagerTest.java
@@ -10,9 +10,9 @@ import org.springframework.core.env.Environment;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
 public class MapManagerTest {
@@ -21,36 +21,91 @@ public class MapManagerTest {
 
     MapManager manager;
 
-    @BeforeEach
-    public void init() throws IOException {
-        File f = new File("C:\\var\\images\\123");
-        Files.deleteIfExists(f.toPath());
-        manager = new MapManager("C:\\var\\maps\\", env);
+//    @BeforeEach
+//    public void init() throws IOException {
+//        File f = new File("C:\\var\\images\\123");
+//        Files.deleteIfExists(f.toPath());
+//        manager = new MapManager("C:\\var\\maps\\", env);
+//    }
+
+    // TODO TESTS
+
+    @Test
+    public void saveRectangleImageThrowsIsNotSquareException() {
+        assertThrows(ImageNotSquareException.class, () -> {
+            // "test" file to B64
+            File directory = new File("./");
+            log.info(directory.getAbsolutePath());
+            File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\squareb64.txt");
+            String b64;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+                b64 = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // save it
+            MapImage mapImage = new MapImage(123, b64);
+            manager.saveImage(mapImage.getId(), mapImage);
+            // check if it is saved
+            String savedB64;
+            try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.jpg"))) {
+                savedB64 = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            assertNotEquals(savedB64, "");
+
+        });
     }
 
     @Test
-    public void saveImage() throws UnprocessableMapImageException, ImageExtensionNotSupportedException, ResolutionTooSmallException, ImageNotSquareException, MapStringIsNotValidBase64Exception {
-        // "test" file to B64
-        File directory = new File("./");
-        log.info(directory.getAbsolutePath());
-        File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\problemb64.txt");
-        String b64;
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
-            b64 = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        // save it
-        MapImage mapImage = new MapImage(123, b64);
-//        byte[] bytes = Base64.getDecoder().decode(b64);
-        manager.saveImage(mapImage.getId(), mapImage);
-        // check if it is saved
-        String savedB64;
-        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.jpg"))) {
-            savedB64 = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        assertNotEquals(savedB64, "");
+    public void saveRectangleImageThrowsResolutionIsTooSmallException() {
+        assertThrows(ResolutionTooSmallException.class, () -> {
+            // "test" file to B64
+            File directory = new File("./");
+            log.info(directory.getAbsolutePath());
+            File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\smallb64.txt");
+            String b64;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+                b64 = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // save it
+            MapImage mapImage = new MapImage(123, b64);
+            manager.saveImage(mapImage.getId(), mapImage);
+            // check if it is saved
+            String savedB64;
+            try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.jpg"))) {
+                savedB64 = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            assertNotEquals(savedB64, "");
+
+        });
     }
+
+    @Test
+    // this checks only invalid characters because there is no way to check for actual image validity
+    public void saveRectangleImageThrowsInvalidBase64Exception() throws ResolutionTooSmallException, ImageNotSquareException, MapStringIsNotValidBase64Exception, UnprocessableMapImageException, ImageExtensionNotSupportedException {
+        assertThrows(MapStringIsNotValidBase64Exception.class, () -> {
+            // "test" file to B64
+            manager = new MapManager("C:\\var\\maps\\", env);
+            File directory = new File("./");
+            log.info(directory.getAbsolutePath());
+            File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\invalidb64.txt");
+            String b64;
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+                b64 = reader.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            // save it
+            MapImage mapImage = new MapImage(1243, b64);
+        });
+    }
+
+
+
 }
