1ab1a1b77a01b981ace492e501fa4c0be88d8f8e
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2023-03-05 00:04:02 +0100

    file writer and manager, tests

--- src/test/java/com/offliner/rust/rust_offliner/map/MapFileWriterTest.java
+++ src/test/java/com/offliner/rust/rust_offliner/map/MapFileWriterTest.java
@@ -1,8 +1,8 @@
-package map;
+package com.offliner.rust.rust_offliner.map;
 
 import com.offliner.rust.rust_offliner.exceptions.*;
 import com.offliner.rust.rust_offliner.maps.MapImage;
-import com.offliner.rust.rust_offliner.maps.MapManager;
+import com.offliner.rust.rust_offliner.maps.MapFileWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,18 +15,18 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Slf4j
-public class MapManagerTest {
+public class MapFileWriterTest {
 
     Environment env;
 
-    MapManager manager;
+    MapFileWriter manager;
 
-//    @BeforeEach
-//    public void init() throws IOException {
-//        File f = new File("C:\\var\\images\\123");
-//        Files.deleteIfExists(f.toPath());
-//        manager = new MapManager("C:\\var\\maps\\", env);
-//    }
+    @BeforeEach
+    public void init() throws IOException {
+        File f = new File("C:\\var\\images\\123");
+        Files.deleteIfExists(f.toPath());
+        manager = new MapFileWriter("C:\\var\\maps\\", env);
+    }
 
     // TODO TESTS
 
@@ -46,14 +46,6 @@ public class MapManagerTest {
             // save it
             MapImage mapImage = new MapImage(123, b64);
             manager.saveImage(mapImage.getId(), mapImage);
-            // check if it is saved
-            String savedB64;
-            try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.jpg"))) {
-                savedB64 = reader.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            assertNotEquals(savedB64, "");
 
         });
     }
@@ -73,15 +65,6 @@ public class MapManagerTest {
             }
             // save it
             MapImage mapImage = new MapImage(123, b64);
-            manager.saveImage(mapImage.getId(), mapImage);
-            // check if it is saved
-            String savedB64;
-            try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.jpg"))) {
-                savedB64 = reader.readLine();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            assertNotEquals(savedB64, "");
 
         });
     }
@@ -91,7 +74,7 @@ public class MapManagerTest {
     public void saveRectangleImageThrowsInvalidBase64Exception() throws ResolutionTooSmallException, ImageNotSquareException, MapStringIsNotValidBase64Exception, UnprocessableMapImageException, ImageExtensionNotSupportedException {
         assertThrows(MapStringIsNotValidBase64Exception.class, () -> {
             // "test" file to B64
-            manager = new MapManager("C:\\var\\maps\\", env);
+            manager = new MapFileWriter("C:\\var\\maps\\", env);
             File directory = new File("./");
             log.info(directory.getAbsolutePath());
             File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\invalidb64.txt");
---
+++ src/test/java/com/offliner/rust/rust_offliner/map/MapManagerTest.java
@@ -0,0 +1,67 @@
+package com.offliner.rust.rust_offliner.map;
+
+import com.offliner.rust.rust_offliner.exceptions.*;
+import com.offliner.rust.rust_offliner.interfaces.IMapDao;
+import com.offliner.rust.rust_offliner.maps.MapImage;
+import com.offliner.rust.rust_offliner.maps.MapManager;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+@Transactional
+@Slf4j
+public class MapManagerTest {
+
+    @Autowired
+    MapManager manager;
+
+    @Autowired
+    IMapDao dao;
+
+    @BeforeEach
+    void init() throws IOException {
+        File f1 = new File("C:\\var\\images\\123.png");
+        File f2 = new File("C:\\var\\images\\123.jpg");
+        Files.deleteIfExists(f1.toPath());
+        Files.deleteIfExists(f2.toPath());
+    }
+
+    @Test
+    void saveWithMapSize() throws ResolutionTooSmallException, ImageNotSquareException, MapStringIsNotValidBase64Exception, UnprocessableMapImageException, PrecedentEntityNotExistsException, ImageExtensionNotSupportedException {
+        long id = 123;
+        File directory = new File("./");
+        log.info(directory.getAbsolutePath());
+        File file = new File("C:\\Users\\estra\\Documents\\rust_offliner\\rust_offliner\\src\\main\\resources\\static\\goodb64.txt");
+        String b64;
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))){
+            b64 = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        // save it
+        MapImage mapImage = new MapImage(id, b64);
+        manager.save(id, mapImage);
+        // check if it is saved
+        String savedB64;
+        try (BufferedReader reader = new BufferedReader(new FileReader("C:\\var\\maps\\123.png"))) {
+            savedB64 = reader.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        assertNotEquals(savedB64, "");
+
+    }
+}
