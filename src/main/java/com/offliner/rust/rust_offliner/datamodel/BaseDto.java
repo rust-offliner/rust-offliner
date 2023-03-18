c2550f55f520cfbcf113c13428a6945e0ab527ea
karczi <k.kozlinka@zsebydgoszcz.onmicrosoft.com>
2023-03-18 18:55:44 +0100

    validation

--- src/main/java/com/offliner/rust/rust_offliner/datamodel/BaseDto.java
+++ src/main/java/com/offliner/rust/rust_offliner/datamodel/BaseDto.java
@@ -2,10 +2,15 @@ package com.offliner.rust.rust_offliner.datamodel;
 
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+
 @Data
 public class BaseDto {
-    private long id;
+    @Min(value = 1, message = "Server id must be greater than 0")
+    private long serverId;
+    @Min(value = 0, message = "Coordinates must be greater than 0")
     private int x;
+    @Min(value = 0, message = "Coordinates must be greater than 0")
     private int y;
     private boolean friendly;
 }
